ef9693965f74a5a8cbc5eef8c2e612508a475e47
Chad Autry <chad.autry@gmail.com>
2019-05-17 19:37:05 -0500

    Tweak the policy page. Update the url, send the policy version back, log errors, allow mulultiple policies

--- src/components/Policy.jsx
+++ src/components/Policy.jsx
@@ -1,18 +1,22 @@
 import { Link, Redirect } from "react-router-dom";
 import React from "react";
 
-const TOS = class TOS extends React.Component {
+//Hard coded policies by default, but would be fairly simple to make a request to fetch them from the server
+const policies = [{name: "Terms of Service", version: "1.0", checked: false, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sagittis dictum libero in efficitur. Ut mattis pharetra sem, id ornare metus iaculis sit amet. Pellentesque et mi a diam viverra varius nec ac magna. In dapibus odio non venenatis blandit. Aenean lacinia vulputate nunc, eget vestibulum justo mattis id. Aliquam eu arcu ut sem varius tristique id sed felis. Curabitur sed libero id nulla cursus volutpat eu eu nulla. Morbi pulvinar, mauris quis mattis porta, metus leo luctus nulla, luctus blandit eros lorem non enim. Duis tristique ornare mi in rhoncus. Praesent non justo varius dolor placerat tincidunt aliquam sit amet ligula. Cras at nulla finibus, mattis dolor non, condimentum orci. Sed blandit tristique pretium. Suspendisse sit amet nunc vestibulum, condimentum lectus a, auctor urna."}
+, {name: "Privacy Policy", version: "2.0", checked: false, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sagittis dictum libero in efficitur. Ut mattis pharetra sem, id ornare metus iaculis sit amet. Pellentesque et mi a diam viverra varius nec ac magna. In dapibus odio non venenatis blandit. Aenean lacinia vulputate nunc, eget vestibulum justo mattis id. Aliquam eu arcu ut sem varius tristique id sed felis. Curabitur sed libero id nulla cursus volutpat eu eu nulla. Morbi pulvinar, mauris quis mattis porta, metus leo luctus nulla, luctus blandit eros lorem non enim. Duis tristique ornare mi in rhoncus. Praesent non justo varius dolor placerat tincidunt aliquam sit amet ligula. Cras at nulla finibus, mattis dolor non, condimentum orci. Sed blandit tristique pretium. Suspendisse sit amet nunc vestibulum, condimentum lectus a, auctor urna."}];
+const Policy = class Policy extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { tosChecked: false };
+    this.state = {policies: policies};
     // This line is important!
-    this.tosCheckClicked = this.tosCheckClicked.bind(this);
+    this.policyCheckClicked = this.policyCheckClicked.bind(this);
   }
 
-  tosCheckClicked() {
-    this.setState({
-      tosChecked: !this.state.tosChecked
-    });
+  policyCheckClicked(index) {
+    //Bit of a hacky way to create a new policies array so we don't mutate the state
+    let newPolicies = JSON.parse(JSON.stringify(this.state.policies));
+    newPolicies[index].checked = !newPolicies[index].checked;
+    this.setState({policies: newPolicies})
   }
 
   render() {
@@ -24,33 +28,58 @@ const TOS = class TOS extends React.Component {
     }
     return (
       <div>
-        <div className="center-form panel">
+          {this.state.policies.map((item, index) => (
+          //This is where the individual policies could be broken down further into another repeated component
+          <div key={index+'policyPanel'} className="panel panel-default">
+          <div className="panel-heading">
+          <h3 className="panel-title">{item.name}</h3>
+          </div>
           <div className="panel-body">
-            <h2 className="text-center">Terms of Service </h2>
+            
             <h6 className="text-center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sagittis dictum libero in efficitur. Ut mattis pharetra sem, id ornare metus iaculis sit amet. Pellentesque et mi a diam viverra varius nec ac magna. In dapibus odio non venenatis blandit. Aenean lacinia vulputate nunc, eget vestibulum justo mattis id. Aliquam eu arcu ut sem varius tristique id sed felis. Curabitur sed libero id nulla cursus volutpat eu eu nulla. Morbi pulvinar, mauris quis mattis porta, metus leo luctus nulla, luctus blandit eros lorem non enim. Duis tristique ornare mi in rhoncus. Praesent non justo varius dolor placerat tincidunt aliquam sit amet ligula. Cras at nulla finibus, mattis dolor non, condimentum orci. Sed blandit tristique pretium. Suspendisse sit amet nunc vestibulum, condimentum lectus a, auctor urna.
+              {item.text}
             </h6>
-            <h3 className="text-center" onClick={this.tosCheckClicked} >
+          </div>
+            <div className="panel-footer">
+            <h3 className="panel-title" onClick={() => this.policyCheckClicked(index)} >
                   Accept:
                  <i
                   className={
-                this.state.tosChecked
+                this.state.policies[index].checked
                     ? "fa fa-check-square-o"
                     : "fa fa-square-o"
                   }
                   />
             </h3>
             
+            </div>
+            </div>
+           ))}
+        <div className="center-form panel">
+          <div className="panel-body">
+            
             <button
               className={
-                this.state.tosChecked
+                this.state.policies.reduce((accumulator, currentValue) => {return accumulator && currentValue.checked;}, true)
                   ? "btn btn-lg btn-block btn-success"
                   : "btn btn-lg btn-block btn-success disabled"
               }
               onClick={() => {
-                  if (this.state.tosChecked) {
-                      this.props.fetchService.getJsonWithAuth('/login', 'application/json', (json) => {
-                       this.props.authService.setToken(json.token);})
+                  if (this.state.policies.reduce((accumulator, currentValue) => {
+                      return accumulator && currentValue.checked;}, true)) {
+                      //Set spinner
+                      this.props.fetchService.getJsonWithAuth('/backend/policyAccepted', 
+                       'application/json', 
+                       (json) => {
+                           //Success
+                           //remove spinner
+                           this.props.authService.setToken(json.token);},
+                       (json) => {
+                           //Failure
+                           console.log(JSON.parse(json));
+                           //remove spinner
+                       },
+                       {"acceptedPolicy":this.state.policies})
                   }
               }}>
               Create User
@@ -66,4 +95,4 @@ const TOS = class TOS extends React.Component {
     );
   }
 };
-export default TOS;
+export default Policy;
